bd4ec8e09b93cbead86f76c2a77cc2f68c5cc288
Kenley Jean <bedjiken@gmail.com>
2019-10-08 12:29:33 -0400

    remove unsue imports

--- hooks/useInterval.ts
+++ hooks/useInterval.ts
@@ -1,23 +1,21 @@
-import React, {useState, useEffect, useRef} from 'react';
-import {type} from "os";
+import { useEffect, useRef } from "react";
 
 export const useInterval = (callback: () => void, delay: number) => {
-    const savedCallback = useRef<(() => void) | null>(null);
+  const savedCallback = useRef<(() => void) | null>(null);
 
-    useEffect(() => {
-            savedCallback.current  = callback;
-    }, [callback]);
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
 
-    useEffect(() => {
-        function tick() {
-            if(savedCallback && savedCallback.current){
-                savedCallback.current();
-            }
-        }
-        if (delay !== null) {
-            let id = setInterval(tick, delay);
-            return () => clearInterval(id);
-        }
-    }, [delay]);
+  useEffect(() => {
+    function tick() {
+      if (savedCallback && savedCallback.current) {
+        savedCallback.current();
+      }
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
 };
-
